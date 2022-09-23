313fd7a0f865ee13748efc026f493ea3e0b539bd
Kim Moonsoo <kiana507680@gmail.com>
2022-09-23 18:10:53 +0900

    2022.09.23 캐릭터 Move
    캐릭터 간단 조작 + 애니메이션 적용

--- InfiniteDungeon/Assets/Scripts/Character_Mei.cs
+++ InfiniteDungeon/Assets/Scripts/Character_Mei.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Character_Mei : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //
+    private Rigidbody rigidbody = null;
+    private Animator animator = null;
 
-    // Update is called once per frame
-    void Update()
+    // Move
+    private float moveSpeed = 3.0f;
+    private float spinSpeed = 360.0f;
+    private float moveInput = 0.0f;
+    private float spinInput = 0.0f;
+
+    private void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + transform.forward * moveInput * moveSpeed * Time.fixedDeltaTime);
+        rigidbody.MoveRotation(rigidbody.rotation * Quaternion.AngleAxis(spinInput * spinSpeed * Time.fixedDeltaTime, Vector3.up));
+    }
+    private void Update()
+    {
+
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 readKey = context.ReadValue<Vector2>();
+        moveInput = readKey.y;
+        spinInput = readKey.x;
+
+        if (context.started)
+        {
+            if(moveInput > 0)
+            {
+                animator.SetBool("Run", true);
+                moveSpeed = 3.0f;
+            }
+            else
+            {
+                animator.SetBool("Walk_B", true);
+                moveSpeed = 1.5f;
+            }
+        }
+        if (context.canceled)
+        {
+            animator.SetBool("Run", false);
+            animator.SetBool("Walk_B", false);
+        }
     }
 }
